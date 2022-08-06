e748055e02d51cbef5985062bd81ae35d8b117a0
364864264 <364864264@qq.com>
2022-08-06 22:06:40 +0800

    提交

--- src/views/admin/basics/user/Edit.vue
+++ src/views/admin/basics/user/Edit.vue
@@ -5,20 +5,20 @@
 </template>
 
 <script>
-  export default {
-    name: 'EditUser',
-    data () {
-      return {}
-    },
-    computed: {},
-    mounted () {},
-    methods: {},
-    components: {},
-    watch: {},
-    errorCaptured (err, vm, info) {
-      console.log(err)
-    },
+export default {
+  name: 'EditUser',
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {},
+  watch: {},
+  errorCaptured (err, vm, info) {
+    console.log(err)
   }
+}
 </script>
 
 <style scoped>
